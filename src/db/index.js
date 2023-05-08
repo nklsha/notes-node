38e6808ecc9ea62657a515d52892c698b6e314a0
Nikhil <123703123+nklsha@users.noreply.github.com>
2023-05-08 17:31:14 +1000

    Updated workflow yaml file with env-var secrets

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,22 @@
 const { Pool } = require("pg");
 
-if (process.env.NODE_ENV !== "prod") { 
-  require("dotenv").config({path: ".env.dev"}); 
-} 
+if (process.env.NODE_ENV !== "prod") {
+  require("dotenv").config({ path: ".env.dev" });
+}
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.DB,
-    password: process.env.POSTGRES_PWD,
-    port: parseInt(process.env.POSTGRES_PORT),
-  });
- 
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PWD,
+  port: parseInt(process.env.POSTGRES_PORT),
+});
+
+pool.connect((err, client, done) => {
+  if (err) throw err
+})
+
 module.exports = {
   query: (text, params) => pool.query(text, params).catch(e => e.stack)
 };
+
